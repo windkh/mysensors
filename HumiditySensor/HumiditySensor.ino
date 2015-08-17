101626e066bda5c7283f51d6e2fb4a623187514d
windkh <karl-heinz.wind@web.de>
2015-08-17 23:01:42 +0200

    Battery mode can be controlled via define now.

--- HumiditySensor/HumiditySensor.ino
+++ HumiditySensor/HumiditySensor.ino
@@ -21,7 +21,7 @@ Widerstand 10K DHT22 PIN2 --> 3.3V
 #include <SPI.h>
 #include <MySensor.h>  
 #include <DHT.h>  
-#include <Battery.h>
+
 
 #define CHILD_ID_HUM 0
 #define CHILD_ID_TEMP 1
@@ -29,74 +29,106 @@ Widerstand 10K DHT22 PIN2 --> 3.3V
 #define HUMIDITY_SENSOR_DIGITAL_PIN 3
 unsigned long SLEEP_TIME = 10000; // Sleep time between reads (in milliseconds)
 
+// #define BATTERY_POWERED
+#ifdef BATTERY_POWERED
+#include <Battery.h>
+Battery battery;
+#endif
+
 
 MySensor gw;
-Battery battery;
 DHT dht;
+
 float lastTemp;
 float lastHum;
-boolean metric = true;
 MyMessage msgHum(CHILD_ID_HUM, V_HUM);
 MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
 
 void setup()
 {
+#ifdef BATTERY_POWERED
 	battery.begin(1200000, 400000);
+#endif
 
 	gw.begin(NULL, AUTO, false);
-	dht.setup(HUMIDITY_SENSOR_DIGITAL_PIN);
-
+	dht.setup(HUMIDITY_SENSOR_DIGITAL_PIN, DHT::AM2302);
+	
 	// Send the Sketch Version Information to the Gateway
-	gw.sendSketchInfo("Humidity", "1.1");
+	gw.sendSketchInfo("Humidity", "1.2");
 
 	// Register all sensors to gw (they will be created as child devices)
 	gw.present(CHILD_ID_HUM, S_HUM);
 	gw.present(CHILD_ID_TEMP, S_TEMP);
+}
 
-	metric = gw.getConfig().isMetric;
+bool temperatureChanged()
+{
+	bool changed = false;
+	float temperature = dht.getTemperature();
+	if (!isnan(temperature))
+	{
+		if (temperature != lastTemp)
+		{
+			lastTemp = temperature;
+			changed = true;
+		}
+
+		Serial.print("T: ");
+		Serial.println(temperature);
+	}
+	else
+	{
+		Serial.println("Failed reading temperature from DHT");
+	}
+
+	return changed;
+}
+
+bool humidityChanged()
+{
+	bool changed = false;
+
+	float humidity = dht.getHumidity();
+	if (!isnan(humidity))
+	{
+		if (humidity != lastHum)
+		{
+			lastHum = humidity;
+			changed = true;
+		}
+
+		Serial.print("H: ");
+		Serial.println(humidity);
+	}
+	else
+	{
+		Serial.println("Failed reading humidity from DHT");
+	}
+
+	return changed;
 }
 
 void loop()
 {
 	delay(dht.getMinimumSamplingPeriod());
-
-	float temperature = dht.getTemperature();
-	if (isnan(temperature))
+	
+	bool tempChanged = temperatureChanged();
+	bool humChanged = humidityChanged();
+	if (tempChanged || humChanged)
 	{
-		Serial.println("Failed reading temperature from DHT");
-	}
-	else if (temperature != lastTemp)
-	{
-		lastTemp = temperature;
-		if (!metric)
-		{
-			temperature = dht.toFahrenheit(temperature);
-		}
-		gw.send(msgTemp.set(temperature, 1));
-		Serial.print("T: ");
-		Serial.println(temperature);
-	}
-
-	float humidity = dht.getHumidity();
-	if (isnan(humidity))
-	{
-		Serial.println("Failed reading humidity from DHT");
-	}
-	else if (humidity != lastHum)
-	{
-		lastHum = humidity;
-		gw.send(msgHum.set(humidity, 1));
-		Serial.print("H: ");
-		Serial.println(humidity);
+		gw.send(msgTemp.set(lastTemp, 1));
+		gw.send(msgHum.set(lastHum, 1));
 	}
 
 	gw.sleep(SLEEP_TIME); //sleep a bit
-
+	
+#ifdef BATTERY_POWERED
 	int batteryPercent;
 	if (battery.checkVoltage(&batteryPercent))
 	{
 		gw.sendBatteryLevel(batteryPercent);
 	}
+#endif
 }
 
 
