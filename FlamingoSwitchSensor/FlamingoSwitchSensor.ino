1725fd124799c48ab41af30ab0934083a6b5a211
windkh <karl-heinz.wind@web.de>
2016-12-29 23:16:01 +0100

    Fixed flamingo sketch. MAde dht optional

--- FlamingoSwitchSensor/FlamingoSwitchSensor.ino
+++ FlamingoSwitchSensor/FlamingoSwitchSensor.ino
@@ -1,24 +1,25 @@
-/* (c) windkh 2015
-MySensors 2.0
+/* (c) windkh 2016
+MySensors 2.1
 Flamingo Switch Sensor (Repeater)
 required hardware:
 - 1x Arduino
 - 1x Radio NRF24L01+
-- (1x 433MHz Receiver) (optional)
+- (1x 433MHz Receiver)	(optional see HAS_RECEIVER)
 - 1x 433Mhz Sender
-- 1x DHT-22
+- 1x DHT-22				(optional see HAS_DHT)
 connect PIN3 to receiver unit Data-PIN
 (connect PIN4 to sender unit Data-PIN) (optional)
 connect PIN5 to DHT-22
 
 The sketch registeres a sensor 0 which reports all received codes. This one can be used to sniff for codes, 
-or to send incomming codes directly into the air.
+or to send incoming codes directly into the air.
 The sensors 1-4 are hardcoded switches. You have to enter your codes from the remote control in the table 
 DEVICE_CODES first. Every button on the remote control sends 4 different keys. Though only one is required 
 the sketch is able to send all 4 after each other. All states are written into the eeprom. On restart the sketch will 
 restore the switch states based on the saved values.
 */
 
+#define BAUDRATE 115200
 #define MY_DEBUG_VERBOSE
 #define MY_SPECIAL_DEBUG
 
@@ -33,20 +34,23 @@ restore the switch states based on the saved values.
 
 #include "FlamingoSwitch.h"
 #include <MySensors.h> 
-#include <SPI.h>
-#include <EEPROM.h>  
+#include <SPI.h> 
 
+// switches to switch off optional parts (e.g. for debugging)
+// #define HAS_RECEIVER 
+#define HAS_DHT 
+
+
+//-----------------------------------------------------------------------------
+// DHT22
+#ifdef HAS_DHT
 #include <DHT.h>  
-
 #include "Timer.h"
-
 
 //-----------------------------------------------------------------------------
 // Timer
 #define SENSOR_UPDATE_INTERVAL 60000
 
-//-----------------------------------------------------------------------------
-// DHT22
 #define CHILD_ID_TEMP 11
 #define CHILD_ID_HUM 12
 #define HUMIDITY_SENSOR_DIGITAL_PIN 5
@@ -60,19 +64,21 @@ MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
 //-----------------------------------------------------------------------------
 // Timer
 Timer timer;
+#endif
 
 //-----------------------------------------------------------------------------
 FlamingoSwitch Switch;
 
 const int TX_PIN  = 4;
-const int IRQ_PIN = 1; // IRQ1 = PIN 3 on Arduino Uno
+const int IRQ_PIN = 1; // IRQ1 = PIN 3 on Arduino Uno see HAS_RECEIVER
 
-#define OFF 0
-#define ON  1
+//#define OFF 0
+//#define ON  1
 
 #define DEVICES	 4				/* Amount of units supported per remote control A-D*/
 #define CODES_PER_DEVICE 4		/* Amount of codes stored per A-D*/
-#define DELAY 50                /* Delay in ms between code transmissions */
+int OnCounter = 0;
+int OffCounter = 0;
 
 // the remote control sends 4 different codes of each state.
 struct Codes
@@ -89,29 +95,27 @@ struct Device
 
 Device DEVICE_CODES[DEVICES] =
 {
-	{
-		{ 0x24DC2060, 0x267C51A0, 0x25C90CE0, 0x277193A0 }, // (28Bit) A OFF
-		{ 0x25B25B60, 0x24613E20, 0x27263DE0, 0x24E77D60 }  // (28Bit) A ON 
+	{ //  0          1          2          3 
+		{ 0x25C90CE, 0x24DC206, 0x277193A, 0x267C51A }, // (28Bit) A OFF
+		{ 0x24E77D6, 0x27263DE, 0x25B25B6, 0x24613E2 }  // (28Bit) A ON 
 	},
 	{
-		{ 0x275BADD0, 0x26933A90, 0x27F31150, 0x2440F0D0 }, // (28Bit) B OFF
-		{ 0x2717B510, 0x2547C990, 0x26585B90, 0x259A0850 }  // (28Bit) B ON 
+		{ 0x275BADD, 0x27F3115, 0x2440F0D, 0x26933A9 }, // (28Bit) B OFF
+		{ 0x2547C99, 0x26585B9, 0x259A085, 0x2717B51 }  // (28Bit) B ON 
 	},
 	{
-		{ 0xE4936D50, 0xE73720D0, 0xE616D1D0, 0xE4D49F50 }, // (28Bit) C OFF
-		{ 0xE56BF910, 0xE56EB710, 0xE62AF390, 0xE4FBCD90 }  // (28Bit) C ON 
+		{ 0xE73720D, 0xE616D1D, 0xE4D49F5, 0xE4936D5 }, // (28Bit) C OFF
+		{ 0xE62AF39, 0xE56BF91, 0xE4FBCD9, 0xE56EB71 }  // (28Bit) C ON 
 	},
 	{
-		{ 0x65B67B60, 0x64C8BFA0, 0x6504C320, 0x6526F4A0 }, // (28Bit) D OFF
-		{ 0x65F1C2A0, 0x670319A0, 0x65E19420, 0x643F9860 }  // (28Bit) D ON 
+		{ 0x65B67B6, 0x64C8BFA, 0x6504C32, 0x6526F4A }, // (28Bit) D OFF
+		{ 0x670319A, 0x65E1942, 0x643F986, 0x65F1C2A }  // (28Bit) D ON 
 	}
 };
 
-#define BAUDRATE 115200
-
-void send(uint32_t code)
+void sendCode(uint32_t code)
 {
-	Serial.print("Sending ");
+	Serial.print("Sending 0x");
 	Serial.print(code, HEX);
 	Serial.println("");
 
@@ -119,7 +123,7 @@ void send(uint32_t code)
 }
 
 // send the state (on/off) to the switch 1..n
-void send(uint8_t device, uint8_t state)
+void sendCode(uint8_t device, uint8_t state)
 {
 	// check if device code between 0-3 (A-D)
 	if (device >= DEVICES)
@@ -144,24 +148,22 @@ void send(uint8_t device, uint8_t state)
 	Serial.print(state);
 	Serial.println();
 
-	if (state == OFF)
+	uint32_t sendBuffer;
+	if (state == 0) // OFF
 	{
-		for (int i = 0; i < CODES_PER_DEVICE; i++)
-		{
-			send(DEVICE_CODES[device].Off.Code[3-i]);
-			delay(DELAY);
-		}
+		int i = OffCounter++ % CODES_PER_DEVICE;
+		sendBuffer = DEVICE_CODES[device].Off.Code[i];
 	}
 	else
 	{
-		for (int i = 0; i < CODES_PER_DEVICE; i++)
-		{
-			send(DEVICE_CODES[device].On.Code[3-i]);
-			delay(DELAY);
-		}
+		int i = OnCounter++ % CODES_PER_DEVICE;
+		sendBuffer = DEVICE_CODES[device].On.Code[i];
 	}
+
+	sendCode(sendBuffer);
 }
 
+#ifdef HAS_DHT
 // timer handler to read and send temperature
 void onTimer()
 {
@@ -200,6 +202,7 @@ void readHumidity()
 		send(msgHum.set(lastHum, 1));
 	}
 }
+#endif
 
 
 // setup serial communication, initialize the pins for communication between arduino and rx and tx units.
@@ -207,13 +210,10 @@ void readHumidity()
 void presentation() 
 {
 	Serial.begin(BAUDRATE);
-	sendSketchInfo("Flamingo Switch", "2.0");
-
-	// setup 433Mhz
-	Switch.enableReceive(IRQ_PIN);
-	Switch.enableTransmit(TX_PIN);
+	sendSketchInfo("Flamingo Switch", "2.1");
 
 	// DHT Sensor
+#ifdef HAS_DHT
 	dht.setup(HUMIDITY_SENSOR_DIGITAL_PIN);
 	present(CHILD_ID_TEMP, S_TEMP);
 	present(CHILD_ID_HUM, S_HUM);
@@ -226,35 +226,42 @@ void presentation()
 		send(message);
 	}
 
+	// Start timer to read hum and temp.
+	//int tickEvent = 
+	timer.every(SENSOR_UPDATE_INTERVAL, onTimer);
+#endif
+
+	// setup 433Mhz
+#ifdef HAS_RECEIVER
+	Switch.enableReceive(IRQ_PIN); <-- not used in this sketch
+#endif
+	Switch.enableTransmit(TX_PIN);
+
 	// id 0..n are predefined switches with hardcoded codes.
 	for (uint8_t i = 0; i < DEVICES; i++)
 	{
 		uint8_t sensorId = i + 1; // sensor 0 is a generic send / receive device
 		present(sensorId, S_LIGHT);
 		bool state = loadState(i);
-		send(i, state); 
+		sendCode(i, state);
 
 		MyMessage message(sensorId, V_LIGHT);
 		message.set(state);
 		send(message);
 	}
-
-	// Start timer to read hum and temp.
-	//int tickEvent = 
-	timer.every(SENSOR_UPDATE_INTERVAL, onTimer);
 }
+
 
 // processes my sensors data. Receives codes and sends them to the mysensors controller.
 void loop()
 {
+#ifdef HAS_RECEIVER
 	if (Switch.available())
 	{
 		unsigned long code = Switch.getReceivedValue();
 		
-		uint32_t state = code << 4; // 28Bit --> 32Bit
-
 		Serial.print("Detected code:");
-		Serial.print(state, HEX);
+		Serial.print(code, HEX);
 		Serial.println("");
 
 		MyMessage message(0, V_VAR1);
@@ -263,8 +270,11 @@ void loop()
 
 		Switch.resetAvailable();
 	}
+#endif
 
+#ifdef HAS_DHT
 	timer.update();
+#endif
 }
 
 // imcomming message handler.
@@ -286,7 +296,7 @@ void receive(const MyMessage& message)
 		Serial.print(state, HEX);
 		Serial.println("");
 
-		send(state);
+		sendCode(state);
 
 		MyMessage message(sensor, V_VAR1);
 		message.set(state);
@@ -294,17 +304,17 @@ void receive(const MyMessage& message)
 	}
 	else
 	{
-		uint8_t sensor = message.sensor;
-		Serial.print("Incoming change for switch:");
-		Serial.print(sensor);
-		Serial.print(", value: ");
-		Serial.print(message.getBool(), DEC);
-		Serial.println("");
-
 		if (message.type == V_LIGHT)
 		{
+			uint8_t sensor = message.sensor;
+			Serial.print("Incoming change for switch:");
+			Serial.print(sensor);
+			Serial.print(", value: ");
+			Serial.print(message.getBool(), DEC);
+			Serial.println("");
+		
 			bool state = message.getBool();
-			send(sensor-1, state); // -1 as we start counting the sensors at 1 instead of 0
+			sendCode(sensor-1, state); // -1 as we start counting the sensors at 1 instead of 0
 			saveState(sensor, state);
 
 			MyMessage message(sensor, V_LIGHT);
