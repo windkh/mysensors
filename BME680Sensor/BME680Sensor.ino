34fe4156e74869672a5fcb55b67aa78745049868
windkh <karl-heinz.wind@web.de>
2017-10-22 11:50:20 +0200

    switched to BME680 library: https://github.com/windkh/BME680_Breakout waiting for pull-request wo be merged into the original library.

--- BME680Sensor/BME680Sensor.ino
+++ BME680Sensor/BME680Sensor.ino
@@ -7,7 +7,7 @@ required hardware:
 - 1x BME680 (https://github.com/watterott/BME680-Breakout)
 
 required libraries:
-  https://github.com/DFRobot/DFRobot_BME680
+  https://github.com/windkh/BME680_Breakout
 
 Notes on AirQuality output:
 IAQ index value is between 0-500
@@ -33,35 +33,33 @@ IAQ index value is between 0-500
 #include <Wire.h>
 #include <SPI.h>
 
-#include "DFRobot_BME680_I2C.h"
+#include <BME680_Library.h>
 
-const uint8_t bme_addr = 0x77;		//bme I2C method address
-DFRobot_BME680_I2C bme(bme_addr);	//I2C method
+BME680_Library bme680;
 
-const float SEALEVEL = 688; // meters abobve sealevel. We assume that this sensor is located on some static place
+bool sendAlways = true; // set this to false if you only want to send values when they are changed.
+
+const float SEALEVEL = 688; // meters above sealevel. We assume that this sensor is located at some static place
 const float SEALEVEL_PRESSURE = 1013.25;
-unsigned long SLEEP_TIME = 10000; // 10
+unsigned long SLEEP_TIME = 2000; // ms
 
 
 // ----------------------------------------------------------------------------
 #define CHILD_ID_PRESSURE 0
 #define CHILD_ID_HUM 1
 #define CHILD_ID_TEMP 2
-#define CHILD_ID_AIRQUALITY 3
-#define CHILD_ID_RESISTANCE 4
+#define CHILD_ID_RESISTANCE 3
 
 boolean metric = true;
 
 float lastHum = -1;
 float lastPressure = -1;
 float lastTemp = -1;
-float lastAirQuality = -1;
 float lastResistance = -1;
 
 MyMessage msgHum(CHILD_ID_HUM, V_HUM);
 MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
 MyMessage pressureMsg(CHILD_ID_PRESSURE, V_PRESSURE);
-MyMessage airQualityMsg(CHILD_ID_AIRQUALITY, V_VAR1);
 MyMessage resistanceMsg(CHILD_ID_RESISTANCE, V_VAR1);
 
 
@@ -74,7 +72,6 @@ void presentation()
 	present(CHILD_ID_PRESSURE, S_BARO);
 	present(CHILD_ID_HUM, S_HUM);
 	present(CHILD_ID_TEMP, S_TEMP);
-	present(CHILD_ID_AIRQUALITY, S_AIR_QUALITY);
 	present(CHILD_ID_RESISTANCE, S_CUSTOM);
 
 	metric = getConfig().isMetric;
@@ -90,101 +87,96 @@ void loop()
 
 void initSensor()
 {
-	if (bme.begin()) {
-		Serial.println("init sucessful");
+	Wire.begin();
+
+	if (!bme680.begin())
+	{
+		Serial.println("Initialization of BME680 failed!");
+		for (;;); // spin forever
 	}
-	else {
-		Serial.println("init faild");
-		for (;;);
+	
+	if (!bme680.configureForcedMode())
+	{
+		Serial.println("Configuring force mode of BME680 failed!");
+		for (;;); // spin forever
 	}
-	Serial.println();
 }
 
 
 // derived from the example code which can be found here
-// https://github.com/DFRobot/DFRobot_BME680/tree/master/examples/DFRobot_BME680_I2C
+// https://github.com/vicatcu/BME680_Breakout/tree/master/BME680_Library/examples/Basic
 void updateSensor(void)
 {
-	bme.startConvert();					  //to get a accurate values
-	Serial.println();
-
-	// Temperature ----------------------------------------------------------------------------------------------------
-	float temp = bme.readTempture();      //float tempture, unit degree Celsius, this is account to two decimal places
-	if (lastTemp != temp)
+	bme680.configureForcedMode();
+	if (bme680.read())
 	{
-		lastTemp = temp;
-		Serial.print(F("Temperature = "));
-		Serial.print(lastTemp);
-		Serial.println(metric ? F("*C") : F("*F"));
-		if (!send(msgTemp.set(lastTemp, 1)))
+		// Temperature ----------------------------------------------------------------------------------------------------
+		float temp = bme680.getTemperature();
+		if (lastTemp != temp || sendAlways)
 		{
-			lastTemp = -1.0;
+			lastTemp = temp;
+			Serial.print(F("Temperature = "));
+			Serial.print(lastTemp);
+			Serial.println(metric ? F("*C") : F("*F"));
+			if (!send(msgTemp.set(lastTemp, 1)))
+			{
+				lastTemp = -1.0;
+			}
+		}
+
+
+		// Humidity -------------------------------------------------------------------------------------------------------
+		float humidity = bme680.getRelativeHumidity();
+		if (lastHum != humidity || sendAlways)
+		{
+			lastHum = humidity;
+			Serial.print(F("Humidity = "));
+			Serial.print(lastHum);
+			Serial.println(F("%"));
+			if (!send(msgHum.set(lastHum, 1)))
+			{
+				lastTemp = -1.0;
+			}
+		}
+
+
+		// Pressure -------------------------------------------------------------------------------------------------------
+		float absolutePressure = bme680.getBarometricPressure();
+		float pressure = getSeaLevelPressure(SEALEVEL, absolutePressure);
+		if (lastPressure != pressure || sendAlways)
+		{
+			lastPressure = pressure;
+			Serial.print(F("sealevel Pressure = "));
+			Serial.print(lastPressure);
+			Serial.println(F("hPa"));
+			if (!send(pressureMsg.set(lastPressure, 1)))
+			{
+				lastPressure = -1.0;
+			}
+		}
+
+
+		// float altitude = bme.readAltitude();  //float altitude, Unit metre, this is account to one decimal places
+		// This is not the absolute Altitude, but one calculated using a global atmospheric formula.
+
+
+		// Gas Resistance -----------------------------------------------------------------------------------------------------
+		float gasResistance = bme680.getGasResistance();
+		if (lastResistance != gasResistance || sendAlways)
+		{
+			lastResistance = gasResistance;
+			Serial.print(F("R = "));
+			Serial.print(lastResistance);
+			Serial.println(F("Ohms"));
+			if (!send(resistanceMsg.set(lastResistance, 1)))
+			{
+				lastResistance = -1.0;
+			}
 		}
 	}
-
-
-	// Humidity -------------------------------------------------------------------------------------------------------
-	float humidity = bme.readHumidity();  //float humidity, Unit relative humidity, this is account to two decimal places
-	if (lastHum != humidity)
+	else
 	{
-		lastHum = humidity;
-		Serial.print(F("Humidity = "));
-		Serial.print(lastHum);
-		Serial.println(F("%"));
-		if (!send(msgHum.set(lastHum, 1)))
-		{
-			lastTemp = -1.0;
-		}
-	}
-
-
-	// Pressure -------------------------------------------------------------------------------------------------------
-	float absolutePressure = bme.readPressure();  //float pressure, Unit Unit MPa, this is account to two decimal places
-	float pressure = getSeaLevelPressure(SEALEVEL, absolutePressure) / 100.0; // hPa
-	if (lastPressure != pressure)
-	{
-		lastPressure = pressure;
-		Serial.print(F("sealevel Pressure = "));
-		Serial.print(lastPressure);
-		Serial.println(F("hPa"));
-		if (!send(pressureMsg.set(lastPressure, 1)))
-		{
-			lastPressure = -1.0;
-		}
-	}
-
-
-	// float altitude = bme.readAltitude();  //float altitude, Unit metre, this is account to one decimal places
-	// This is not the absolute Altitude, but one calculated using a global atmospheric formula.
-
-	// AirQuality -----------------------------------------------------------------------------------------------------
-	uint16_t gas = bme.readGas();         //uint16_t gas, Unit ppm, this is account to one decimal places
-	if (lastAirQuality != gas)
-	{
-		lastAirQuality = gas;
-		Serial.print(F("gas = "));
-		Serial.print(lastAirQuality);
-		Serial.println(F(" ppm")); // <-- Note: maybe this unit is wrong
-
-		// Note: the value read from register 0x2A seems to be a raw gas resistance which is a 10Bit Value
-		if (!send(airQualityMsg.set(lastAirQuality, 1)))
-		{
-			lastAirQuality = -1.0;
-		}
-	}
-
-	// Resistance -----------------------------------------------------------------------------------------------------
-	float gasResistance = bme.readGasResistance();  //float resistance, Unit ohm, this is account to two decimal places
-	if (lastResistance != gasResistance)
-	{
-		lastResistance = gasResistance;
-		Serial.print(F("R = "));
-		Serial.print(lastResistance);
-		Serial.println(F(" Ohm"));
-		if (!send(resistanceMsg.set(lastResistance, 1)))
-		{
-			lastResistance = -1.0;
-		}
+		Serial.println("Reading from BME680 failed!");
 	}
 }
 
